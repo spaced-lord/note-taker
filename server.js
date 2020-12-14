be59ce6c36f68fdd993197bc6f472b1cb8d96e4a
spaced-lord <zackarynoe@gmail.com>
2020-12-13 19:12:23 -0700

    Finish delete

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const { parse } = require('path');
+const shortid = require('shortid');
 
 // Set up express
 const PORT = process.env.PORT || 3000   
@@ -12,17 +13,17 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 // Connect HTMl, js and CSS
-app.use(expres.static(path.join(_dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 //Routes
 // GET new note request
-app.get('/', (req, res) => res.sendFile(path.join(_dirname, '/public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 // GET new notes
-app.get('/notes', (req, res) => res.sendFile(path.join(_dirname, '/public/notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 // GET saved notes
 app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(_dirname, "/db/db/.json"));    
+    res.sendFile(path.join(__dirname, "/db/db/.json"));    
 });
 
 
@@ -30,14 +31,14 @@ app.get('/api/notes', (req, res) => {
 app.post('/api/notes', (req, res) => {
     let newNote = req.body;
     newNote.id = shortid.generate();
-    fs.readFile(path.join(_dirname, '/db/db.json'), (err,data) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), (err,data) => {
         if (err) {
             console.log(error);
         } else {
             let storedData = JSON.parse(data);
             storedData.push(newNote);
             fs.writeFile(
-                path.join(_dirname, "/db/db.json"),
+                path.join(__dirname, "/db/db.json"),
                 JSON.stringify(storedData),
                 (err) => {
                     if (err) {
@@ -51,7 +52,34 @@ app.post('/api/notes', (req, res) => {
     res.end();
 });
 
-app.listen.(PORT, () => console.log(`You are tuned to PORT ${PORT}`));
+
+app.delete('/api/notes/:id', (req, res) => {
+    let selectId = req.params.id;
+    fs.readFile(path.join(__dirname, '/db/db.json'), (err, data) => {
+        if (err) {
+            console.log(error);
+        } else {
+            let storedData = JSON.parse(data);
+            for (let i = 0; i < storedData.length; i++) {
+                if (storedData[i].id === selectId) {
+                    storedData.splice(i, i);
+                }
+            }
+            fs.writeFile(
+                path.join(__dirname, '/db/db.json'),
+                JSON.stringify(storedData),
+                (err) => {
+                    if (err) {
+                        console.log(error);
+                    }
+                }
+            );
+        }
+    });
+    res.end();
+});
+
+app.listen(PORT, () => console.log(`You are tuned to PORT ${PORT}`));
 
 
 
